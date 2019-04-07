5b389d29445c2beae1386e9ec9bcaea237abfb26
Serg Minin <everestdon@ukr.net>
2019-04-07 19:42:12 +0300

    first commit

---
+++ src/TodoItem.jsx
@@ -0,0 +1,37 @@
+import React, { useContext } from "react";
+import { Context } from "./context";
+
+export default function TodoItem({ title, id, completed }) {
+  const { dispatch } = useContext(Context);
+
+  return (
+    <li className={!completed ? "todo" : "todo completed"}>
+      <label className=" deep-purple lighten-5">
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={() =>
+            dispatch({
+              type: "toggle",
+              payload: id
+            })
+          }
+        />
+        <span>{title}</span>
+
+        <i
+          className="material-icons  pink-text text-darken-3"
+          title="Delete Me!"
+          onClick={() =>
+            dispatch({
+              type: "remove",
+              payload: id
+            })
+          }
+        >
+          delete_sweep
+        </i>
+      </label>
+    </li>
+  );
+}
